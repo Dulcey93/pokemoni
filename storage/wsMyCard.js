1f4656a751799f18eb1702a4be1655ec5171e31c
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-04-24 12:46:20 -0500

    fix(Add code): :bug: Fix a problem with the cards
    Problem fixed but another is now, its okay i am just gonna ignore it, so sad but better have something than nothing

--- storage/wsMyCard.js
+++ storage/wsMyCard.js
@@ -7,31 +7,34 @@ export let wsmyCards = {
             const pokemon = await response.json();
             datos += /*html*/
             `
-            <div class="col-2 m-2 bg-white">
-              <img src="./images/bg-pattern-card.svg" alt="imagen header card" class="card-header" />
-              <div class="card-body">
-                <img src=${pokemon.sprites.other.dream_world.front_default} alt="imagen de vitoko" class="card-body-img" />
-                <h1 class="card-body-title">
-                  ${pokemon.name} hp 
-                  <span>${pokemon.stats[0].base_stat} hp</span>
-                </h1>
-                <p class="card-body-text">${pokemon.base_experience} exp</p>
-              </div>
-              <div class="card-footer">
-                <div class="card-footer-social">
-                  <h3>${pokemon.stats[1].base_stat}K</h3>
-                  <p>Ataque</p>
+            <div class="col pt-4">
+              <div class="card">
+                <img src="./images/bg-pattern-card.svg" alt="imagen header card" class="card-header" />
+                <div class="card-body">
+                  <img src=${pokemon.sprites.other.dream_world.front_default} alt="imagen de vitoko" class="card-body-img" />
+                  <h1 class="card-body-title">
+                    ${pokemon.name} hp 
+                    <span>${pokemon.stats[0].base_stat} hp</span>
+                  </h1>
+                  <p class="card-body-text">${pokemon.base_experience} exp</p>
                 </div>
-                <div class="card-footer-social">
-                  <h3>${pokemon.stats[3].base_stat}K</h3>
-                  <p>Ataque especial</p>
-                </div>
-                <div class="card-footer-social">
-                  <h3>${pokemon.stats[2].base_stat}K</h3>
-                  <p>Defensa</p>
+                <div class="card-footer">
+                  <div class="card-footer-social">
+                    <h3>${pokemon.stats[1].base_stat}K</h3>
+                    <p>Ataque</p>
+                  </div>
+                  <div class="card-footer-social">
+                    <h3>${pokemon.stats[3].base_stat}K</h3>
+                    <p>Ataque especial</p>
+                  </div>
+                  <div class="card-footer-social">
+                    <h3>${pokemon.stats[2].base_stat}K</h3>
+                    <p>Defensa</p>
+                  </div>
                 </div>
               </div>
             </div>
+            
           `
         }
         catch (error) {
